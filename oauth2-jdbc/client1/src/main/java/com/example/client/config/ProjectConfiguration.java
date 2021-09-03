4d8877a4f1619313a4251b16f067140a7272d10b
kleitonrufino <kleiton.arufino@gmail.com>
2021-09-03 16:48:34 -0300

    -

--- oauth2-jdbc/client1/src/main/java/com/example/client/config/ProjectConfiguration.java
+++ oauth2-jdbc/client1/src/main/java/com/example/client/config/ProjectConfiguration.java
@@ -5,17 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -57,30 +56,45 @@ public class ProjectConfiguration extends WebSecurityConfigurerAdapter {
 		return cr;
 	}
 
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
+	
+	@Bean
+	public OAuth2AuthorizedClientManager authorizedClientManager(
+			ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientService clientService) {
 
-  @Bean
-  public OAuth2AuthorizedClientManager authorizedClientManager(
-          ClientRegistrationRepository clientRegistrationRepository,
-          OAuth2AuthorizedClientRepository authorizedClientRepository) {
+		OAuth2AuthorizedClientProvider authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+				.clientCredentials().build();
 
-    OAuth2AuthorizedClientProvider authorizedClientProvider =
-            OAuth2AuthorizedClientProviderBuilder.builder()
-                    .clientCredentials()
-                    .build();
+		AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+				clientRegistrationRepository, clientService);
+		authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
 
-    var authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
-                                          clientRegistrationRepository,
-                                          authorizedClientRepository);
-
-    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
-
-    return authorizedClientManager;
-  }
-  
+		return authorizedClientManager;
+	}	
+	
+//  @Bean
+//  public RestTemplate restTemplate() {
+//    return new RestTemplate();
+//  }
+//
+//  @Bean
+//  public OAuth2AuthorizedClientManager authorizedClientManager(
+//          ClientRegistrationRepository clientRegistrationRepository,
+//          OAuth2AuthorizedClientRepository authorizedClientRepository) {
+//
+//    OAuth2AuthorizedClientProvider authorizedClientProvider =
+//            OAuth2AuthorizedClientProviderBuilder.builder()
+//                    .clientCredentials()
+//                    .build();
+//
+//    var authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+//                                          clientRegistrationRepository,
+//                                          authorizedClientRepository);
+//
+//    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+//
+//    return authorizedClientManager;
+//  }
+//  
   @Bean
   WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
       ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
